34e63fbc6e43c560295fa370b392e1b6575c9274
Youssef Hassan <youssef.hassan.prof@gmail.com>
2025-03-24 05:22:00 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,33 @@
+
+var quotes = [
+    {
+      quote: "Be yourself; everyone else is already taken",
+      author: "--Oscar Wilde",
+  },
+   {
+      quote: "Do not take life too seriously. You will not get out alive.",
+      author: "--Elbert Hubbard",
+   },
+    {
+      quote:"A room without books is like a body without a soul",
+      author: "--Marcus Tullius Cicero",
+    },
+     {
+      quote: "Be the change that you wish to see in the world",
+      author: "--Mahatma Gandhi",
+     },
+     {
+        quote: "It's not what happens to you, but how you react to it that matters.",
+        author: "--Epictetus"
+     }
+    ]
+  
+
+document.getElementById("demo").onclick = function(){
+    var index = Math.floor(Math.random() * quotes.length);
+    document.getElementById("quote").innerHTML =  quotes[index].quote;
+    document.getElementById("author").innerHTML = quotes[index].author;
+
+};
+
+
